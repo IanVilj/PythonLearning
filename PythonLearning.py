148e34290230c1dfcf74f84596bdf2b5a7619c0d
Ian Viljehamn <ian.viljehamn@gmail.com>
2024-09-28 14:40:30 -0400

    organized code and added some things making fun of my cats

--- PythonLearning.py
+++ PythonLearning.py
@@ -2,26 +2,28 @@ print("Hello Bwee Bwee")
 print(5+5)
 print("5+5 equals 10")
 print(100/5) #end of useless part
-x = 100-10
+x, y, a, b = 100-10, 1050-102, 2035817-1203, 21230358-223
 print(x)
-y = 1050-102
 print(y)
 print(y-x)
 if x > 5:
     print("X is more than 5!")
 if y < x:
     print("Y is less than X!") #dunno why i even have this in here cause it doesn't do anything
-a = 2035817-1203
-b = 21230358-223
 print(a)#checking to see if code was working
 print(b)
 print(a+b)
 print(x+y)#end of checking
 if a+b > x+y:
     print("A+B is more than x+y!")
-H = "johnny"
+H, h = "johnny", "appleseed"
 print(H)
-h = " appleseed"
 print(h)
 print(H+h)
 #Guys i am like a god at coding!!
+BweeBweeIsTheBest, FatAssCat, Sarga = "Fish breath attack", " Candy", " MEOOOOOOOOOWW"
+print(BweeBweeIsTheBest)
+print(FatAssCat)
+print(Sarga)
+print(BweeBweeIsTheBest+FatAssCat+Sarga)
+#end of making fun of my cats
