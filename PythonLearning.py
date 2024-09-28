4af4e92ce1cb030f63adba70e2cecf6acfc38fa3
Ian Viljehamn <ian.viljehamn@gmail.com>
2024-09-28 00:39:19 -0400

    changed the wording of some comments

--- PythonLearning.py
+++ PythonLearning.py
@@ -1,7 +1,7 @@
 print("Hello Bwee Bwee")
 print(5+5)
 print("5+5 equals 10")
-print(100/5) #end of useless part 9/27/2024
+print(100/5) #end of useless part
 x = 100-10
 print(x)
 y = 1050-102
@@ -10,7 +10,7 @@ print(y-x)
 if x > 5:
     print("X is more than 5!")
 if y < x:
-    print("Y is less than X!") #dunno why i even have this in here cause it doesn't do anything, 9/27/2024
+    print("Y is less than X!") #dunno why i even have this in here cause it doesn't do anything
 a = 2035817-1203
 b = 21230358-223
 print(a)#checking to see if code was working
