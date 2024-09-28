88c7fd460f0315a8dd7d7b9bda85c63c59faded2
Ian Viljehamn <ian.viljehamn@gmail.com>
2024-09-27 23:51:47 -0400

    added comments

--- PythonLearning.py
+++ PythonLearning.py
@@ -1,7 +1,7 @@
 print("Hello Bwee Bwee")
 print(5+5)
 print("5+5 equals 10")
-print(100/5)
+print(100/5) #end of useless part 9/27/2024
 x = 100-10
 print(x)
 y = 1050-102
@@ -10,4 +10,4 @@ print(y-x)
 if x > 5:
     print("X is more than 5!")
 if y < x:
-    print("Y is less than X!")
+    print("Y is less than X!") #dunno why i even have this in here cause it doesn't do anything, 9/27/2024
