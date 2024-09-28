8017c8a371ad18a22a65a60ff8e5bc212aba0781
Ian Viljehamn <ian.viljehamn@gmail.com>
2024-09-28 00:36:25 -0400

    added a fun experiment, checks to see if some code was working, and comments.

--- PythonLearning.py
+++ PythonLearning.py
@@ -11,3 +11,11 @@ if x > 5:
     print("X is more than 5!")
 if y < x:
     print("Y is less than X!") #dunno why i even have this in here cause it doesn't do anything, 9/27/2024
+a = 2035817-1203
+b = 21230358-223
+print(a)#checking to see if code was working
+print(b)
+print(a+b)
+print(x+y)#end of checking
+if a+b > x+y:
+    print("A+B is more than x+y!")
